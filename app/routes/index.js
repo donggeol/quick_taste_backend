f2b66aba8cf5b11ee63b77b9f59ec9e93d683186
Euiseok Kim <ikess0330@gmail.com>
2017-07-27 14:21:03 +0900

    naver blog searching is implemented. need to parse the result.
    Signed-off-by: Euiseok Kim <ikess0330@gmail.com>

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,6 @@
-const noteRoutes = require('./note_routes');
+const tempRoutes = require('./temp_routes');
 
 module.exports = function(app, db) {
-    noteRoutes(app, db);
+    tempRoutes(app, db);
     // Other route groups could go here, in the future
 };
