2bb94f3b9961246cf37cd3b5c705aedf1b7d2aeb
Euiseok Kim <ikess0330@gmail.com>
2017-08-05 10:53:58 +0900

    search_naver by POST
    Signed-off-by: Euiseok Kim <ikess0330@gmail.com>

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -25,6 +25,8 @@ module.exports = function(app, db) {
         var host = 'openapi.naver.com';
         var port = 443;
         var uri = '/v1/search/blog.xml?';
+            
+        console.log('[GET] keyword : ' + search);
 
         var options = {
             host: host,
@@ -34,9 +36,45 @@ module.exports = function(app, db) {
             headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
         };
 
+        console.log(req.body)
+        
         req = https.request(options, function(response) {
             console.log('STATUS: ' + res.statusCode);
-            console.log('HEADERS: ' + JSON.stringify(res.headers));
+            response.setEncoding('utf8');
+            response.on('data', function (xml) {
+                parseString(xml, function(err, result){
+                    var data = JSON.stringify(result);
+                    res.send(data);
+                });
+            });
+        });
+        req.end();
+    });
+    
+    app.post('/search_naver', (req, res) => {
+        var search = req.body.keyword;
+        var queryOption = {'query':search, 'display':10, 'start':1, 'sort':'sim'};
+        var query = querystring.stringify(queryOption);
+        var client_id = 'A9PK0LPb6izP6EWhLnM5';
+        var client_secret = 'aesZzF3PKh';
+        var host = 'openapi.naver.com';
+        var port = 443;
+        var uri = '/v1/search/blog.xml?';
+            
+        console.log('[POST] keyword : ' + search);
+
+        var options = {
+            host: host,
+            port: port,
+            path: uri + query,
+            method: 'GET',
+            headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+        };
+
+        console.log(req.body)
+        
+        req = https.request(options, function(response) {
+            console.log('STATUS: ' + res.statusCode);
             response.setEncoding('utf8');
             response.on('data', function (xml) {
                 parseString(xml, function(err, result){
