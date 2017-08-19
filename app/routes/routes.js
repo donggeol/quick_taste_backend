9b26467f7cb862fc8374c4f9bca4af379a804900
Euiseok Kim <ikess0330@gmail.com>
2017-08-19 16:36:09 +0900

    get_store_info
    Signed-off-by: Euiseok Kim <ikess0330@gmail.com>

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,7 +1,7 @@
 var https = require('https');
 var querystring = require('querystring');
 var parseString = require('xml2js').parseString;
-var cheerio = require("cheerio"); 
+var cheerio = require("cheerio");
 var request = require("request");
 
 module.exports = function(app, db) {
@@ -88,67 +88,270 @@ module.exports = function(app, db) {
         req.end();
     });
 
+    app.get('/get_store_info', (req, res) => {
+        var query = req.query.keyword;
+        var url = 'https://m.map.naver.com/search2/search.nhn?';
+        var path = 'query=' + encodeURIComponent(query);
+        var serverData = '', _serverData = '', __serverData = '';
+        var result = '';
 
-    app.post('/get_store_info', (req, res) => {
-        var query = req.body.keyword;
-        var url = 'https://search.naver.com/search.naver?';
-        var path = 'query=' + query;
-
-        console.log('[POST] get store info of : ' + query);
-
-        //options = {
-        //    hostname: 'search.naver.com',
-        //    port: 443,
-        //    path: '/search.naver?',
-        //    method: 'GET'
-        //};
+        console.log('[POST] get map list of : ' + query);
+        console.log('url : ' + url + path);
 
         request({
-            url: url+path,
-            //proxy: 'http://www.rapidproxy.org/'
+            url: url + path,
+            mutipart:[{
+                'content-type' :'text/html;charset=utf-8'
+            }],
         }, function (error, response, body) {
             if (error) {
                 console.log(error)
             } else {
-                console.log(response)
+                console.log('success');
             }
-        });
+        })
+        .on('data', function(data) {
+            serverData += data;
+        })
+        .on('response', function(response) {
+            response.setEncoding('utf8');
+            response.on('data', function(data) {
+                console.log('received ' + data.length + ' bytes of compressed data')
+            })
+            response.on('end', function() {
+                var $ = cheerio.load(serverData);
 
-        //req = https.request(options, function (response) {
-        //    get_store_info_of(response, res);
-        //});
+                var start = serverData.indexOf("\"id\"");
+                var end = serverData.indexOf(",", start+1);
+                var id = serverData.substring(start+8,end-1);
 
-        //req.on('error', (e) => {
-        //    console.error(e);
-        //});
+                console.log("id -> " + id);
+                console.log("url -> " + 'https://store.naver.com/restaurants/detail?id=' + id);
 
-        //req.end();
+                request({
+                    url: 'https://store.naver.com/restaurants/detail?id=' + id
+                }, function (error, response, body) {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        console.log('success');
+                    }
+                })
+                .on('data', function(data) {
+                    _serverData += data;
+                })
+                .on('response', function(response) {
+                    response.on('data', function(data) {
+                        //console.log('received ' + data.length + ' bytes of compressed data')
+                    })
+                    response.on('end', function() {
+                        var $ = cheerio.load(_serverData);
+
+                        biztel = $("div.list_item_biztel").text();
+                        _biztel = biztel.replace(/(^\s+|\s+$)/g, "");
+
+                        bizaddr = $("div.list_item_address");
+
+                        var mapaddr = '';
+                        map = $(".link");
+                        map.each( function() {
+                            var link = $(this).attr('href');
+                            if ($(this).text() == '지도보기'){
+                                mapaddr = link;
+                            }
+                        });
+
+                        end = bizaddr.text().indexOf("지번");
+                        bizaddr = bizaddr.text().substring(0,end);
+                        _bizaddr = bizaddr.replace(/(^\s+|\s+$)/g, "");
+
+
+                        biztime = $("div.biztime").text();
+                        _biztime = biztime.replace(/(^\s+|\s+$)/g, "");
+
+                        console.log("biztel -> " + _biztel);
+                        console.log("bizaddr -> " + _bizaddr);
+                        console.log("mapaddr -> " + mapaddr);
+                        console.log("biztime -> " + _biztime);
+
+                        result = new Object();
+                        result.biztel = _biztel;
+                        result.bizaddr = _bizaddr;
+                        result.mapaddr = mapaddr;
+                        result.biztime = _biztime;
+
+                        request({
+                            url: 'https://store.naver.com/restaurants/detail?id=' + id + '&photoType=menu'
+                        }, function (error, response, body) {
+                            if (error) {
+                                console.log(error)
+                            } else {
+                                console.log('success');
+                            }
+                        })
+                        .on('data', function(data) {
+                            __serverData += data;
+                        })
+                        .on('response', function(response) {
+                            response.on('data', function(data) {
+                                //console.log('received ' + data.length + ' bytes of compressed data')
+                            })
+                            response.on('end', function() {
+                                var $ = cheerio.load(__serverData);
+
+                                var menu_link = '';
+                                thumb = $("img");
+                                thumb.each( function() {
+                                    var link = $(this).attr('src');
+                                    if (link.indexOf('m1000') != -1){
+                                        menu_link = link;
+                                    }
+                                });
+
+                                result.menu_link = menu_link;
+                                var jsonResult = JSON.stringify(result);
+                                res.send(jsonResult);
+                            })
+                        })
+
+                    })
+                })
+            })
+
+        })
+    });
+
+    app.post('/get_store_info', (req, res) => {
+        var query = req.body.keyword;
+        var url = 'https://m.map.naver.com/search2/search.nhn?';
+        var path = 'query=' + encodeURIComponent(query);
+        var serverData = '', _serverData = '', __serverData = '';
+        var result = '';
+
+        console.log('[POST] get map list of : ' + query);
+        console.log('url : ' + url + path);
+
+        request({
+            url: url + path,
+            mutipart:[{
+                'content-type' :'text/html;charset=utf-8'
+            }],
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log('success');
+            }
+        })
+        .on('data', function(data) {
+            serverData += data;
+        })
+        .on('response', function(response) {
+            response.setEncoding('utf8');
+            response.on('data', function(data) {
+                console.log('received ' + data.length + ' bytes of compressed data')
+            })
+            response.on('end', function() {
+                var $ = cheerio.load(serverData);
+
+                var start = serverData.indexOf("\"id\"");
+                var end = serverData.indexOf(",", start+1);
+                var id = serverData.substring(start+8,end-1);
+
+                console.log("id -> " + id);
+                console.log("url -> " + 'https://store.naver.com/restaurants/detail?id=' + id);
+
+                request({
+                    url: 'https://store.naver.com/restaurants/detail?id=' + id
+                }, function (error, response, body) {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        console.log('success');
+                    }
+                })
+                .on('data', function(data) {
+                    _serverData += data;
+                })
+                .on('response', function(response) {
+                    response.on('data', function(data) {
+                        //console.log('received ' + data.length + ' bytes of compressed data')
+                    })
+                    response.on('end', function() {
+                        var $ = cheerio.load(_serverData);
+
+                        biztel = $("div.list_item_biztel").text();
+                        _biztel = biztel.replace(/(^\s+|\s+$)/g, "");
+
+                        bizaddr = $("div.list_item_address");
+
+                        var mapaddr = '';
+                        map = $(".link");
+                        map.each( function() {
+                            var link = $(this).attr('href');
+                            if ($(this).text() == '지도보기'){
+                                mapaddr = link;
+                            }
+                        });
+
+                        end = bizaddr.text().indexOf("지번");
+                        bizaddr = bizaddr.text().substring(0,end);
+                        _bizaddr = bizaddr.replace(/(^\s+|\s+$)/g, "");
+
+
+                        biztime = $("div.biztime").text();
+                        _biztime = biztime.replace(/(^\s+|\s+$)/g, "");
+
+                        console.log("biztel -> " + _biztel);
+                        console.log("bizaddr -> " + _bizaddr);
+                        console.log("mapaddr -> " + mapaddr);
+                        console.log("biztime -> " + _biztime);
+
+                        result = new Object();
+                        result.biztel = _biztel;
+                        result.bizaddr = _bizaddr;
+                        result.mapaddr = mapaddr;
+                        result.biztime = _biztime;
+
+                        request({
+                            url: 'https://store.naver.com/restaurants/detail?id=' + id + '&photoType=menu'
+                        }, function (error, response, body) {
+                            if (error) {
+                                console.log(error)
+                            } else {
+                                console.log('success');
+                            }
+                        })
+                        .on('data', function(data) {
+                            __serverData += data;
+                        })
+                        .on('response', function(response) {
+                            response.on('data', function(data) {
+                                //console.log('received ' + data.length + ' bytes of compressed data')
+                            })
+                            response.on('end', function() {
+                                var $ = cheerio.load(__serverData);
+
+                                var menu_link = '';
+                                thumb = $("img");
+                                thumb.each( function() {
+                                    var link = $(this).attr('src');
+                                    if (link.indexOf('m1000') != -1){
+                                        menu_link = link;
+                                    }
+                                });
+
+                                result.menu_link = menu_link;
+                                var jsonResult = JSON.stringify(result);
+                                res.send(jsonResult);
+                            })
+                        })
+
+                    })
+                })
+            })
+
+        })
     });
 };
 
-function get_store_info_of(response, res) {
-
-    var serverData = '';
-    response.on('data', function (chunk) {
-        serverData += chunk;
-    });
-
-    response.on('end', function () {
-        var $ = cheerio.load(serverData);
-
-        more_icon = $("div.footer_wrap").text();
-
-        more_icon_ = more_icon.replace(/(^\s+|\s+$)/g, "");
-        
-        console.log("Find by class : api_more_icon -> " + more_icon);
-        console.log("Find by class : trimmed -> " + more_icon_);
-
-        result = $("#footer").text();                     
-        result2 = result.replace(/(^\s+|\s+$)/g, "");     
-        console.log("Find by id : text_0 -> " + result2);
-        
-        res.send(serverData);
-    });
-
-    //return "store info"
-}
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser    = require('body-parser');
 
 const app           = express();
 
-const port = 4000;
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
